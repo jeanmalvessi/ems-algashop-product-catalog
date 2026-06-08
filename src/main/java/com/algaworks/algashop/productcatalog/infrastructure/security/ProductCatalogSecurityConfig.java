4f157e9147e5bb4c763b1d4b3c11906ef304d396
Jean Malvessi <jeanmalvessi@gmail.com>
2026-06-08 17:11:44 -0300

    ProductCatalog security filters

--- src/main/java/com/algaworks/algashop/productcatalog/infrastructure/security/ProductCatalogSecurityConfig.java
+++ src/main/java/com/algaworks/algashop/productcatalog/infrastructure/security/ProductCatalogSecurityConfig.java
@@ -2,6 +2,7 @@ package com.algaworks.algashop.productcatalog.infrastructure.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,7 +19,22 @@ public class ProductCatalogSecurityConfig {
             .cors(cors -> cors.disable())
             .sessionManagement(sessionManagement ->
                     sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/products/*/withdraw", "/api/v1/products/*/restock")
+                            .hasAuthority("SCOPE_products:stock:write")
+                        .requestMatchers(HttpMethod.GET, "/api/v1/products/**")
+                            .hasAuthority("SCOPE_products:read")
+                        .requestMatchers(HttpMethod.POST, "/api/v1/products/**")
+                            .hasAuthority("SCOPE_products:write")
+                        .requestMatchers(HttpMethod.GET, "/api/v1/categories/**")
+                            .hasAuthority("SCOPE_categories:read")
+                        .requestMatchers(HttpMethod.POST, "/api/v1/categories/**")
+                            .hasAuthority("SCOPE_categories:write")
+                        .requestMatchers("/actuator/health")
+                            .permitAll()
+                        .anyRequest()
+                            .authenticated()
+                )
             .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
 
         return http.build();

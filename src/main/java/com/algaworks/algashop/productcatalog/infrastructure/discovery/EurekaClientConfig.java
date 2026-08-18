39c3e84acbdec92c278ae538e442611afd122986
Jean Malvessi <frwk.jean@unimedpoa.com.br>
2026-08-18 17:45:36 -0300

    ProductCatalog Eureka client

---
+++ src/main/java/com/algaworks/algashop/productcatalog/infrastructure/discovery/EurekaClientConfig.java
@@ -0,0 +1,9 @@
+package com.algaworks.algashop.productcatalog.infrastructure.discovery;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDiscoveryClient
+public class EurekaClientConfig {
+}

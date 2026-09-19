3eea58304ee00cd27d627f69cca10aa7b4dbc703
Jean Malvessi <jeanmalvessi@gmail.com>
2026-09-19 11:26:21 -0300

    Sending events to topics and partitions on producer with Kafka template

--- src/main/java/com/algaworks/algashop/productcatalog/application/IntegrationEventPublisher.java
+++ src/main/java/com/algaworks/algashop/productcatalog/application/IntegrationEventPublisher.java
@@ -1,5 +1,5 @@
 package com.algaworks.algashop.productcatalog.application;
 
 public interface IntegrationEventPublisher {
-    void send(Object event, String destination);
+    void send(Object event, String key, String destination);
 }
